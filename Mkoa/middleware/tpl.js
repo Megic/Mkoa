92b4a6522f3744080a7ed34c197c3dedc2a20b02
megic <420636220@qq.com>
2016-05-03 14:58:00 +0800

    删除无用文件

--- Mkoa/middleware/tpl.js
+++ Mkoa/middleware/tpl.js
@@ -1,18 +1,7 @@
 module.exports = function(app){
-    var  baseRender = require('koa-ejs')//ejs模板解析
-        , json = require('koa-json');//json输出
+    var  json = require('koa-json');//json输出
     /////////////////////////////////////////////////静态文件处理///////////////////////////
-    //定义模板
-    //baseRender(app, {
-    //    root: $C.ROOT,
-    //    layout: false,
-    //    viewExt: 'html',
-    //    cache: false,
-    //    debug: $C.logger
-    //});
     var views = require('koa-views');
-
-// Must be used before any router is used
     app.use($F.convert(views($C.ROOT, {
       //  viewExt: 'html',
         map: {
