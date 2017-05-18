5cb159b3f63ab015ceeb944b40de49be34d66f7a
megic <k7727719>
2017-05-18 13:03:19 +0800

    修改logger中间件，增强log4配置

--- Mkoa/middleware/logger.js
+++ Mkoa/middleware/logger.js
@@ -1,13 +1,34 @@
 module.exports = function(app){
     "use strict";
     //输出logger
-        let logger = require('koa-log4js');
-        if(!$C.loger_config){
-            app.use($F.convert(logger()));
-        }
+    const log4js = require('./logger/index');
+    let config={
+        appenders: [{type: 'console'}],
+        replaceConsole: true
+    };
+    if($C.loger_config){
+        if($C.loger_config.file){
+            config={
+                appenders: [
+                    {
+                        type: 'console'
+                    },
+                    {
+                        type: 'file',
+                        filename: $C.loger_config.file,
+                        maxLogSize: $C.loger_config.size || 10*1024*1024,
+                        backups: $C.loger_config.backups || 4,
+                        category: [ 'http','console' ]
+                    }
+                ],
+                replaceConsole: true
+            };
+        }else{config=$C.loger_config}
 
-        if($C.loger_config){
-            app.use($F.convert(logger($C.loger_config)));
-        }
+    }
+    log4js.configure(config);
+    app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }));
+
+
 
 };
